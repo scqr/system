e932f35a6c6b8f89e2a7fcc4b2645fe2fb721cb9
tree@global <tree-global@phone.com>
2018-03-17 21:56:41 +0800

    3.17

--- assets/javascripts/zy.js
+++ assets/javascripts/zy.js
@@ -17,8 +17,22 @@ function addPrefixZero(num) {
     }
     return num;
 }
+//发布内容
+function send() {
+    var sWrite = $('#write').val();
+    var sBoxInput = $('.boxInput').val();
+    var sData = $('.data').val();
+    if(sWrite === '' || sBoxInput === '' || sData === '') {
+        alert('亲，标题、内容和发布时间都不可以为空哦！');
+    } else {
+        var oNewLi = document.createElement('li');
+        oNewLi.innerHTML = '<h3 class="h3">'+ sBoxInput +'<a title="删除" href="javascript:;" class="no">&times;</a></h3><div class="contentP">'+ sWrite +'</div><p class="contentData">' + '发布时间：' + sData + '</p>';
+        $('.contentUls').append(oNewLi);
+        $('#empty').css({"display":"none"});
+    }
+}
 $(function ($) {
-    /*login*/
+    /*login-data*/
     $("#loginData").html(formatDateToString());
     setInterval(function () {
         $("#loginData").html(formatDateToString());
@@ -42,10 +56,41 @@ $(function ($) {
         $(".loginUls").css('display', 'none');
     });
 
-    /*section*/
+    /*section-left*/
     $('li').click(function (ev) {
         ev.stopPropagation();
     }).has('ul').click(function () {
         $(this).toggleClass('b').children('ul').stop(true).toggle('normal');
     });
+
+    /*section-right*/
+    /*$('.no').on("click",function () {
+        $(this).parent().parent().css({"display":"none"});
+    });*/
+    $(".no").on("click",function(event){
+        var target = $(event.target);
+        console.log(target.parent().parent());
+        target.parent().parent().css({"display":"none"});
+    });
+    $('#no1').on("click",function () {
+        $('#empty').css({"display":"none"});
+    });
+    $('#right-btn').on("click",function () {
+        $('#empty').css({"display":"none"});
+    });
+    $('.sectionButton').on("click",function () {
+        $('#empty').css({"display":"block"});
+        $('#box').animate({top:"0"}, 1000);
+    });
+    $('#left-btn').on("click", function () {
+        send();
+    });
+    /*//通过事件委托，将A的删除委托给ul
+    $('.contentUls').on("click",function (ev) {
+        var e  = ev || window.event;
+        var oT = e.target ||e.srcElement;
+        if (oT.nodeName === 'A') {
+            $('.contentUls').remove(oT.parentNode);
+        }
+    });*/
 });
